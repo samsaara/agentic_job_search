c9b7327457981954a3b90931a1bc4aa9c9865c4b
Vivek Vaddina <6368653+samsaara@users.noreply.github.com>
2025-08-26 09:58:45 +0530

    🔧 Update config

--- src/config.py
+++ src/config.py
@@ -1,22 +1,24 @@
 import logging
 from pathlib import Path
 
-file_handler = logging.FileHandler('logs.log')
-file_handler.setLevel(logging.DEBUG)
-
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-file_handler.setFormatter(formatter)
-
-log = logging.Logger('agentic_search')
-log.setLevel(logging.DEBUG)
-log.addHandler(file_handler)
+JOB_TOPIC = "Data Science or Machine/Deep Learning or NLP/LLMs or AI"
+LOG_LEVEL = 'DEBUG'
 
 
 SCRAPE_ORGS_PATH = "src/scrape/orgs.yaml"
-JOB_TOPIC = "Data Science or Machine/Deep Learning or NLP/LLMs or AI"
-
 SCRAPE_DOWNLOAD_PATH = Path("src/scrape/crawl")
 JOBS_WRITE_PATH = Path("src/scrape/jobs")
 
 for path in (SCRAPE_DOWNLOAD_PATH, JOBS_WRITE_PATH):
     path.mkdir(parents=True, exist_ok=True)
+
+
+file_handler = logging.FileHandler('logs.log')
+file_handler.setLevel(LOG_LEVEL)
+
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+
+log = logging.Logger('agentic_search')
+log.setLevel(LOG_LEVEL)
+log.addHandler(file_handler)
