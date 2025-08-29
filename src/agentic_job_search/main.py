2efb8ae988b7ae2b9feeb9f1c8c09557fe9ef215
Vivek Vaddina <6368653+samsaara@users.noreply.github.com>
2025-08-29 15:36:51 +0530

    🧑‍💻 Add option to call crew sync/async

--- src/agentic_job_search/main.py
+++ src/agentic_job_search/main.py
@@ -17,15 +17,28 @@ warnings.filterwarnings("ignore") #, category=SyntaxWarning, module="pysbd")
         wait=wait_exponential(2, min=4, max=300),
         stop=stop_after_attempt(3)
 ) """
-async def _run_async():
+async def _run_async(**kwargs):
     try:
         inputs = await prepare_inputs()
-        crew = AgenticJobSearch().crew()
+        crew = AgenticJobSearch(**kwargs).crew()
         return await crew.kickoff_for_each_async(inputs=inputs)
     except Exception as e:
-        raise Exception(f"An error occurred while running the crew: {e}")
+        raise Exception(f"An error occurred while running the crew {e}")
 
+def _run(**kwargs):
+    try:
+        inputs = asyncio.run(prepare_inputs())
+        crew = AgenticJobSearch(**kwargs).crew()
+        return crew.kickoff_for_each(inputs=inputs)
+    except Exception as e:
+        raise Exception(f"An error occurred while running the crew {e}")
 
 def run():
-    results = asyncio.run(_run_async())
+    kwargs = {
+        'provider': 'OLLAMA',
+        'temperature': 0.1,
+        'max_rpm': None,
+    }
+    # results = asyncio.run(_run_async(**kwargs))
+    results = _run(**kwargs)
     store_final_jobs_report(results)
