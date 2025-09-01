44d2fd23055ca056cba0c9c472f8f34682fbb0ef
Vivek Vaddina <6368653+samsaara@users.noreply.github.com>
2025-09-01 09:05:28 +0530

    🔧 update logs config

--- src/config.py
+++ src/config.py
@@ -4,21 +4,30 @@ from pathlib import Path
 # Change this to whatever you're interested in.
 JOB_TOPIC = "Data Science or Machine/Deep Learning or NLP/LLMs or AI"
 
-LOG_LEVEL = 'DEBUG'
-
-SCRAPE_ORGS_PATH = "src/scrape/orgs.yaml"
+SCRAPE_ORGS_PATH = Path("src/scrape/orgs.yaml")
 SCRAPE_DOWNLOAD_PATH = Path("src/scrape/crawl")
 JOBS_WRITE_PATH = Path("src/scrape/jobs")
 
 for path in (SCRAPE_DOWNLOAD_PATH, JOBS_WRITE_PATH):
     path.mkdir(parents=True, exist_ok=True)
 
-file_handler = logging.FileHandler('logs.log')
-file_handler.setLevel(LOG_LEVEL)
 
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-file_handler.setFormatter(formatter)
+def get_logger(LOG_LEVEL="INFO"):
+    LOG_PATH = Path("logs.log")
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
-log = logging.Logger('agentic_search')
-log.setLevel(LOG_LEVEL)
-log.addHandler(file_handler)
+    log = logging.Logger('agentic_search')
+    log.setLevel(LOG_LEVEL)
+
+    file_handler = logging.FileHandler(LOG_PATH)
+    file_handler.setLevel(LOG_LEVEL)
+    file_handler.setFormatter(formatter)
+
+    log.addHandler(file_handler)
+    
+    return log
+
+
+log = get_logger()
+
+
